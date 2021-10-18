63ba37836da360664c5bdc05c7c7e9173f751b96
harinigadiraju <92371681+harinigadiraju@users.noreply.github.com>
2021-10-18 19:55:41 +0530

    Update area.py

--- area.py
+++ area.py
@@ -1,6 +1,6 @@
 #program to input the radius of a circle
 #from the user and print the area
 r = input("Input the radius of the circle : ")
-int(r)
+float(r)
 a = 3.14 * r**2
 print("The area of the circle with radius "+r+" is : "+a)
