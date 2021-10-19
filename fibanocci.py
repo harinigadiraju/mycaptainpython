547af118673c188b78a38655e3fc49b293826dc7
harinigadiraju <92371681+harinigadiraju@users.noreply.github.com>
2021-10-20 00:57:01 +0530

    Create fibanocci.py

---
+++ fibanocci.py
@@ -0,0 +1,15 @@
+n = int(input("enter number of terms in the series"))
+n1 = 0
+n2 = 1
+i = 0
+if( n < = 0) :
+  print("only positive integers accepted")
+else :
+  print("the fibanocci series with {} terms :".format(n))
+  while ( i < n ) :
+    print( n1)
+    n3 = n1 + n2 
+    n1 = n2
+    n2 = n3
+    i = i + 1
+    
