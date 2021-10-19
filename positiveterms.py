e9e4f158641bec4c06374de85b251dc2a78513a8
harinigadiraju <92371681+harinigadiraju@users.noreply.github.com>
2021-10-20 00:12:41 +0530

    Update positiveterms.py

--- positiveterms.py
+++ positiveterms.py
@@ -4,7 +4,9 @@ n = int(input("enter the number of elements in the list : "))
 for i in range(0,n):
     element = int(input())
     list.append(element)
-    if(listp[i]>0)
+for i in range(0,n):
+    if(list[i]>0):
         listp.append(element)
+
 print("list1 : {}".format(list)")
 print ("list2 : {}".format(listp)")
