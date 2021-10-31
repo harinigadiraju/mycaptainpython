b61f9e453614a3096071327e40ffa36221c73064
harinigadiraju <92371681+harinigadiraju@users.noreply.github.com>
2021-10-31 19:03:16 +0530

    Update frequency.py

--- frequency.py
+++ frequency.py
@@ -1,12 +1,15 @@
 letters = {}
+l1 = {}
+
+
 def most_frequent():
-    s = input("Enter a string : ")
+    s = input("Enter a string")
     s.lower()
     for i in s:
         if i in letters:
             letters[i] += 1
         else:
             letters[i] = 1
-    print(letters)
+    l1 = sorted(letters.items(), key=lambda x: x[1], reverse=True)
 
 most_frequent()
